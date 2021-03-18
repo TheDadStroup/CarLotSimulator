91f2ea637ee89d18df8c1070043c66d6ce123c69
The Dad <Shihen1611@gmail.com>
2021-03-18 18:18:27 -0500

    fixing problem

--- CarLotSimulatorApp/CarLotSimulator/Program.cs
+++ CarLotSimulatorApp/CarLotSimulator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CarLotSimulator
 {
@@ -8,13 +9,59 @@ namespace CarLotSimulator
         {
             //TODO
 
-            //Create a seperate class file called Car
-            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
-            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()
-            //The methods should take one string parameter: the respective noise property
+            //Create a seperate class file called Car   DONE
+
+
+            //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable  DONE
+
+
+            //Car shall have the following methods: MakeEngineNoise(), MakeHonkNoise()  DONE
+
+
+            //The methods should take one string parameter: the respective noise property DONE
+
 
 
             //Now that the Car class is created we can instanciate 3 new cars
+
+            var lot = new CarLot();
+            
+
+
+            var billsCar = new Car();
+            billsCar.Make = "Triumph";
+            billsCar.Model = "Spitfire";
+            billsCar.Year = 1972;
+            billsCar.EngineNoise = "purr";
+            billsCar.HonkNoise = "War Eagle";
+            billsCar.IsDrivable = true;
+
+            lot.Cars.Add(billsCar);
+
+
+            var raysCar = new Car()
+            {
+
+                Year = 1982,
+                Make = "Nissan",
+                Model = "280Z",
+                EngineNoise = "Brmmp",
+                HonkNoise = "Honk!",
+                IsDrivable = false
+            };
+            lot.Cars.Add(raysCar);
+            var whitsCar = new Car(2007, "Jeep", "Wrangler", "uuuga", "vrooom", true);
+            lot.Cars.Add(whitsCar);
+
+            whitsCar.MakeEngineNoise();
+            raysCar.MakeHonkNoise();
+            billsCar.MakeEngineNoise();
+
+            foreach (var car  in lot.Cars)
+            {
+                Console.WriteLine($" Year: {car.Year} Make: {car.Make} Model: {car.Model}");
+            }
+
             //Set the properties for each of the cars
             //Call each of the methods for each car
 
---
+++ CarLotSimulatorApp/CarLotSimulator/car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace CarLotSimulator
+{
+    public class Car
+    {
+        public Car()
+        {
+        }
+
+        public Car(int year,
+            string make,
+            string model,
+            string honkNoise,
+            string engineNoise,
+            bool isDrivable)
+        {
+            Year = year;
+            Make = make;
+            Model = model;
+            HonkNoise = honkNoise;
+            EngineNoise = engineNoise;
+            IsDrivable = isDrivable;
+        }
+
+           
+
+
+        //Car shall have the following properties: Year, Make, Model, EngineNoise, HonkNoise, IsDriveable
+
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string EngineNoise { get; set; }
+        public bool IsDrivable { get; set; }
+        public string HonkNoise { get; set; }
+
+        
+        public void MakeEngineNoise()
+        {
+            Console.WriteLine(EngineNoise);
+        }
+        public void MakeHonkNoise()
+        {
+            Console.WriteLine(HonkNoise);
+        }
+
+    }
+}
